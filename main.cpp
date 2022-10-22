e2d84627541465e4c7a1dc3c5312950554f7fbe8
csherill <csherill@student.21-school.ru>
2022-10-22 17:08:08 +0300

    save progress

--- main.cpp
+++ main.cpp
@@ -17,12 +17,13 @@ struct s_vec {
     float       y;
 };
 
-bool    isValidZeroVec(s_vec compare, s_vec vecTemp)
+s_vec    compare = {-1};
+s_obj    firstObj = {-1};
+
+bool    isValidZeroVec(s_vec vecTemp)
 {
     if((compare.x == 0 && vecTemp.x != 0) || (compare.y == 0 && vecTemp.y != 0))
         return false;
-    else if((compare.y == 0 && vecTemp.x == 0 ) || (compare.x == 0 && vecTemp.y == 0))
-        return true;
     else if(compare.y == 0)
     {
         // vecTemp || compare => compare == (k * vecTemp.x) : (k * vecTemp.y)  >> where k = compare.x / vecTemp.x
@@ -39,59 +40,86 @@ bool    isValidZeroVec(s_vec compare, s_vec vecTemp)
     return true;
 }
 
-bool    isValidVec(s_vec compare, s_vec vecTemp){
-    if (vecTemp.x / compare.x == vecTemp.y / compare.y)
+bool    isValidVec(s_vec vecTemp){
+    if (vecTemp.x * compare.y == vecTemp.y * compare.x)
        return (true);
     return (false);
 }
 
 int algCol(int **crd, int type)
 {
-    static s_vec    compare = {-1};
-    static s_obj    firstObj = {-1};
     s_vec           vecTemp = {};
-
-    if (firstObj.type == -1){
-        if(type == 1)
-            firstObj = { 1, (float)crd[0][0] + ((float)(crd[3][0] - crd[0][0])/2),
-                         (float)crd[1][1] + ((float)(crd[2][1] - crd[1][1])/2)};
-        else
-            firstObj = {(char)0,(float)crd[0][0], (float)crd[0][1]};
-        return FIRST;
+    if (type == 1) {
+        float y = (float) (crd[1][1] - crd[0][1]) / 2;
+        vecTemp = {2, (float) ((float) crd[0][0] + ((float) (crd[3][0] - crd[0][0]) / 2) - firstObj.x),
+                   (float) ((float) crd[0][1] + ((float) (crd[1][1] - crd[0][1]) / 2) - firstObj.y)};
     }
-    else if (compare.type == -1)
-    {
-        if(type == 1)
-            compare = {1, (float)crd[0][0] + ((float)(crd[3][0] - crd[0][0])/2) - firstObj.x,
-                       (float)crd[1][1] + ((float)(crd[2][1] - crd[1][1])/2) - firstObj.y};
-        else
-            compare = {0, (float)crd[0][0] - firstObj.x, (float)crd[0][1] - firstObj.y};
-        return FIRST;
-    }
-    else if(compare.x == 0 || compare.y == 0){
-        if (type == 1)
-            vecTemp = {2, (float)((float)crd[0][0] + ((float)(crd[3][0] - crd[0][0]) / 2) - firstObj.x),
-                    (float)((float)crd[1][1] + ((float) (crd[2][1] - crd[1][1]) / 2) - firstObj.y)};
-        else
-            vecTemp = {2, (float)crd[0][0] - firstObj.x, (float)crd[0][1] - firstObj.y};
-        return (isValidZeroVec(compare,vecTemp));
-    }
-    else {
-        if (type == 1)
-            vecTemp = {2, (float)((float)crd[0][0] + ((float)(crd[3][0] - crd[0][0]) / 2) - firstObj.x),
-                       (float) ((float)crd[1][1] + ((float) (crd[2][1] - crd[1][1]) / 2) - firstObj.y)};
-        else
-            vecTemp = {2, (float)crd[0][0] - firstObj.x, (float)crd[0][1] - firstObj.y};
-        return (isValidVec(compare,vecTemp));
-    }
+    else
+        vecTemp = {2, (float)crd[0][0] - firstObj.x, (float)crd[0][1] - firstObj.y};
+    if(vecTemp.x == 0 && vecTemp.y == 0)
+        return true;
+    else if(compare.x == 0 || compare.y == 0)
+        return (isValidZeroVec(vecTemp));
+    else
+        return (isValidVec(vecTemp));
 }
+
+int findCompareVector(std::ifstream *inFile, int **crd, int *count)
+{
+    int type;
+    for(;(*count) != 0;(--*count)){
+        if(!((*inFile) >> type))
+            return 2;
+        if (type == 1){
+            (*inFile) >> (crd)[0][0];
+            (*inFile) >> (crd)[0][1];
+            (*inFile) >> (crd)[1][0];
+            (*inFile) >> (crd)[1][1];
+            (*inFile) >> (crd)[2][0];
+            (*inFile) >> (crd)[2][1];
+            (*inFile) >> (crd)[3][0];
+            (*inFile) >> (crd)[3][1];
+            if (firstObj.type == -1) {
+                firstObj = {1, (float) crd[0][0] + (float)((float)(crd[3][0] - crd[0][0]) / 2),
+                            (float) crd[0][1] + ((float)(crd[1][1] - crd[0][1])/2) };
+            }
+            else if(compare.type == -1){
+                float x = ((float)crd[3][0] - (float)crd[0][0])/2;
+                float y = ((float)(crd[1][1] - crd[0][1])/2);;
+                compare = {-1, (float)crd[0][0] + ((float)(crd[3][0] - crd[0][0])/2) - firstObj.x,
+                           (float)crd[0][1] + y - firstObj.y};
+                if(compare.y == 0 && compare.x == 0)
+                    compare = {-1};
+                else
+                    break;
+            }
+        }
+        else if (type == 0){
+            (*inFile) >> (crd)[1][0];
+            (*inFile) >> (crd)[0][0];
+            (*inFile) >> (crd)[0][1];
+            if (firstObj.type == -1)
+                firstObj = {(char) 0, (float) crd[0][0], (float) crd[0][1]};
+            else if(compare.type == -1){
+                compare = {1, (float)crd[0][0] - firstObj.x, (float)crd[0][1] - firstObj.y};
+                if(compare.y == 0 && compare.x == 0)
+                    compare = {-1};
+            } else
+                break;
+        if (compare.type != -1)
+            break;
+        }
+    }
+    return true;
+}
+
 
 int main()
 {
-//    std::ifstream           inFile("input.txt");
-//    std::ofstream           outFile("output.txt");
-    std::ifstream           inFile("../input.txt");
-    std::ofstream           outFile("../output.txt");
+    std::ifstream           inFile("input.txt");
+    std::ofstream           outFile("output.txt");
+//    std::ifstream           inFile("../input.txt");
+//    std::ofstream           outFile("../output.txt");
     int                     countObj;
     int                     type;
     int                     **crd;
@@ -108,7 +136,9 @@ int main()
         crd = new int*[4];
         for(int i = 0; i < 4; ++i)
             crd[i] = new int[2];
-        while(countObj--) {
+        if (findCompareVector(&inFile, crd, &countObj) == 2 || countObj == 0)
+            status = 1;
+        for(;countObj != 0; countObj--) {
             if(!(inFile >> type))
                 break;
             if (type == 1){
@@ -132,9 +162,7 @@ int main()
                 break;
             }
             status = algCol((crd), type);
-            if(status == FIRST)
-                continue;
-            else if (status == 1)
+            if (status == 1)
                 continue;
             else {
                 outFile << "No";
